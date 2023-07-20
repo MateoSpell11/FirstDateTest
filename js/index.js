3e5e007b933115c7caafb5d3740c67dff69a2a57
MateoSpell11 <114446237+MateoSpell11@users.noreply.github.com>
2023-07-20 16:24:01 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const documentReady = () => {
   const heroButtonNo = document.querySelector('#heroButtonNo');
 
   const nowZomosNobios = () => {
-    alert('💞 lo sabía 💞');
+    alert('💞 lo sabía chingada madre💞');
     alert('😱Lugar, fecha y hora para ir por ti😱');
     location.href = 'https://www.youtube.com/shorts/A8_AuLDBmKc';
   };
@@ -17,7 +17,7 @@ const documentReady = () => {
     heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
   };
 
-  const partner = prompt('🥰 Nombre 🥰');
+  const partner = prompt('🥰 Requisitos cumplidos?: Detalles:- tiene que estudiar algo [9 semestre y eran 8 ajajaj] - cero de fiestas o trago [mi unico vicio es messi] - que le guste el deporte [viva el fulbo]  - que le guste los videojuegos [más que vivir]  - que podamos jugar videojuegos [a pesar de lo manco] - que le guste lo tranqui [siempre] - que me regale legos o carritos hotweels [hay colección] - que trabaje [trabajando duro o durando en el trabajo] - que no me ignore [difícil ignorarte siendo tan genial] - que le guste el café como a mi [claro que shi] - y que sea un apoyo mutuo 🥰 [siempre] '); 
   heroTitle.innerHTML += partner + ' ❤';
 
   heroButtonSi.addEventListener('click', nowZomosNobios);
