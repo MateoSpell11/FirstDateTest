94a0b82fe91f1b2b3b4f32c9b4a38e901d535e2d
MateoSpell11 <114446237+MateoSpell11@users.noreply.github.com>
2023-07-20 16:08:56 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const documentReady = () => {
   const heroButtonNo = document.querySelector('#heroButtonNo');
 
   const nowZomosNobios = () => {
-    alert('💞 lo sabía 💞');
+    alert('💞 lo sabía chingada madre💞');
     alert('😱Lugar, fecha y hora para ir por ti😱');
     location.href = 'https://www.youtube.com/shorts/A8_AuLDBmKc';
   };
@@ -17,8 +17,19 @@ const documentReady = () => {
     heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
   };
 
-  const partner = prompt('🥰 Nombre 🥰');
-  heroTitle.innerHTML += partner + ' ❤';
+  const partner = prompt('🥰 Requisitos cumplidos?: Detalles:\n
+  - tiene que estudiar algo [9 semestre y eran 8 ajajaj] \n
+  - cero de fiestas o trago [mi unico vicio es messi] \n
+  - que le guste el deporte [viva el fulbo] \n
+  - que le guste los videojuegos [más que vivir]  \n
+  - que podamos jugar videojuegos [a pesar de lo manco] \n
+  - que le guste lo tranqui [siempre] \n
+  - que me regale legos o carritos hotweels [hay colección] \n
+  - que trabaje [trabajando duro o durando en el trabajo] \n
+  - que no me ignore [difícil ignorarte siendo tan genial] \n
+  - que le guste el café como a mi [claro que shi] \n
+  - y que sea un apoyo mutuo 🥰 [siempre] '); 
+//  heroTitle.innerHTML +=  + ' ❤';
 
   heroButtonSi.addEventListener('click', nowZomosNobios);
   heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
