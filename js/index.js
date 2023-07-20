ce2e841dbfded4f963c880a65423c26c13af9275
MateoSpell11 <114446237+MateoSpell11@users.noreply.github.com>
2023-07-20 16:14:02 -0500

    Update working index.js

--- js/index.js
+++ js/index.js
@@ -17,20 +17,9 @@ const documentReady = () => {
     heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
   };
 
-  const partner = prompt('🥰 Requisitos cumplidos?: Detalles:\n
-  - tiene que estudiar algo [9 semestre y eran 8 ajajaj] \n
-  - cero de fiestas o trago [mi unico vicio es messi] \n
-  - que le guste el deporte [viva el fulbo] \n
-  - que le guste los videojuegos [más que vivir]  \n
-  - que podamos jugar videojuegos [a pesar de lo manco] \n
-  - que le guste lo tranqui [siempre] \n
-  - que me regale legos o carritos hotweels [hay colección] \n
-  - que trabaje [trabajando duro o durando en el trabajo] \n
-  - que no me ignore [difícil ignorarte siendo tan genial] \n
-  - que le guste el café como a mi [claro que shi] \n
-  - y que sea un apoyo mutuo 🥰 [siempre] '); 
-  heroTitle.innerHTML += partner  + ' ❤';
 
+  const partner = prompt('🥰 Nombre 🥰');
+  heroTitle.innerHTML += partner + ' ❤';
   heroButtonSi.addEventListener('click', nowZomosNobios);
   heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
 };
